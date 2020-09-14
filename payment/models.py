d364e6ab9d9fc0e190d9f4d85e39e1a8f8649058
zzy <1246456493@qq.com>
2020-09-15 00:20:29 +0800

    bianxie

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Alipay(models.Model):
+    order_id=models.IntegerField('订单编号',primary_key=True)
+    state_payment=models.CharField('支付状态',max_length=20)
+    contract_status=models.CharField('签约状态',max_length=20)
+    created_time=models.DateTimeField('订单创建时间',auto_now_add=True)
+    updated_time=models.DateTimeField('订单修改时间',auto_now=True)
+    # house_profile=models.OneToOneField(House,on_delete=models.CASCADE)
+    # user_profile=models.ForeignKey(User,on_delete=models.CASCADE)
+
